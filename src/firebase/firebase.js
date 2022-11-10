75be57b350dc6ceeb5c363c30efe5f6789c8cebe
MatiasFollenti <matiasfollenti@upc.edu.ar>
2022-11-10 19:33:09 -0300

    Agrego firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3FBJgg9kv5Ama-f0LTVZITVvVr8jQATQ",
+  authDomain: "accesorios-manzanita.firebaseapp.com",
+  projectId: "accesorios-manzanita",
+  storageBucket: "accesorios-manzanita.appspot.com",
+  messagingSenderId: "986109178313",
+  appId: "1:986109178313:web:303cbad85b81fa1ba7f818"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
